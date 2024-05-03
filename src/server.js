606f84fccb8623baf068d093fac08fe810cee23c
Sunghun Yoon <gnsals6004@gmail.com>
2024-05-03 19:41:57 +0900

    feat: live chat

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -9,4 +11,31 @@ app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+// app.listen(3000, handleListen);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+const sockets = [];
+
+wss.on("connection", (socket) => {
+	sockets.push(socket);
+	socket["nickname"] = "Anon";
+	console.log("Connected to Browser ✅");
+	socket.on("close", () => console.log("Disconnected from the Browser ❌"));
+	socket.on("message", (msg) => {
+		const message = JSON.parse(msg.toString());
+		switch (message.type) {
+			case "new_message":
+				sockets.forEach((aSocket) =>
+					aSocket.send(`${socket.nickname}: ${message.payload}`)
+				);
+				break;
+			case "nickname":
+				socket["nickname"] = message.payload;
+				break;
+		}
+	});
+});
+
+server.listen(3000, handleListen);
